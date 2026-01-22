4e72063effd5bfa76c2c22ac39d15323303e3c87
oliverr <oliverr@mit.edu>
2026-01-22 13:45:18 -0500

    12/22/26

--- install.sh
+++ install.sh
@@ -10,14 +10,10 @@ mkdir -p ~/.config/tmux/
 cp tmux.conf ~/.config/tmux/tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
-# tmux -L tmux-tmp new-session -d
-# tmux source-file ~/.config/tmux/tmux.conf
-# tmux run-shell "~/.config/tmux/plugins/tpm/bin/install_plugins"
-# tmux -L 
-
-tmux -L tpm source-file ~/.tmux.conf \; \
-     run-shell "~/.tmux/plugins/tpm/bin/install_plugins" \; \
-     kill-server
+tmux -L tmux-tmp new-session -d
+tmux source-file ~/.config/tmux/tmux.conf
+tmux run-shell "~/.config/tmux/plugins/tpm/bin/install_plugins"
+tmux kill-session -s tmux-tmp
 
 ## Nvim Installation
 ARCHITECTURE=$(uname -m)
