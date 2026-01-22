3d26cc416b20257236d440a6c08ca0e679aa121e
oliverr <oliverr@mit.edu>
2026-01-22 13:31:28 -0500

    Powerline-fonts

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
 
+rm -rf ~/.config/{tmux,nvim}
+
+
 ## Tmux Installation
 sudo apt install -y tmux
 mkdir -p ~/.config/tmux/
 cp tmux.conf ~/.config/tmux/tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 tmux source ~/.config/tmux/tmux.conf
-bash ~/.config/tmux/plugins/tpm/scripts/update_plugin.sh
+~/.config/tmux/plugins/tpm/scripts/update_plugin.sh
 
 
 ## Nvim Installation
@@ -29,4 +32,5 @@ cp vimtmux.lua ~/.config/nvim/lua/plugins/vimtmux.lua
 
 
 ## Setup PS1
+sudo apt install -y fonts-powerline
 echo "PS1='\[\e[48;5;239m\] staff@maslab \[\e[0;38;5;239m\]${TRIANGLE}\[\e[0m\] \w \[\e[38;5;112m\]\$\[\e[0m\] '" >> ~/.bashrc
